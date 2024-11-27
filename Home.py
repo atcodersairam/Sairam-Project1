c04ea341d1d37d4105081177fd330a9150bea9ad
BN Sairam <139829666+atcodersairam@users.noreply.github.com>
2024-11-27 21:12:03 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-# Add a hyperlink
-st.markdown('[Click here to visit the Streamlit app](https://code-execution-modul-compiler-sairam-project1.streamlit.app/)')
+# Add a hyperlink with custom text
+st.markdown('[Code Execution and Testing](https://code-execution-modul-compiler-sairam-project1.streamlit.app/)')
