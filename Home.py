bdf4335de919b19bd62ead6d21997661fabec752
BN Sairam <139829666+atcodersairam@users.noreply.github.com>
2024-11-27 21:11:07 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-# Add a hyperlink with custom text
-st.markdown('[Code Execution and Testing](https://code-execution-modul-compiler-sairam-project1.streamlit.app/)')
+# Add a hyperlink that opens in the same tab
+st.markdown('<a href="https://code-execution-modul-compiler-sairam-project1.streamlit.app/" target="_self">Code Execution and Testing</a>', unsafe_allow_html=True)
