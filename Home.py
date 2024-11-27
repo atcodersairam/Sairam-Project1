cdf71396d4acc12752ded1a99c1b7bae693261db
BN Sairam <139829666+atcodersairam@users.noreply.github.com>
2024-11-27 19:35:10 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,7 +10,6 @@ st.write("Click the button below to execute and test the code!")
 if st.button("Code Execution and Testing"):
     # Redirect to the provided URL when clicked
     st.markdown(
-        f'<a href="https://code-execution-modul-compiler-sairam-project1.streamlit.app/" target="_blank">'
         f'<button style="background-color: #007bff; color: white; border-radius: 5px; padding: 15px 30px; font-size: 18px; font-weight: bold; border: none;">'
         f'Code Execution and Testing</button></a>',
         unsafe_allow_html=True
