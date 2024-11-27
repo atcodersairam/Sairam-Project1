a195caf5f0e6da8939fb6fd323e0cc31a04411a5
BN Sairam <139829666+atcodersairam@users.noreply.github.com>
2024-11-27 21:13:47 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-# Add a hyperlink with custom text
-st.markdown('[Code Execution and Testing](https://code-execution-modul-compiler-sairam-project1.streamlit.app/)')
- 
+# Hyperlink that triggers a redirect
+url = "https://code-execution-modul-compiler-sairam-project1.streamlit.app/"
+st.markdown('[Code Execution and Testing](#)', unsafe_allow_html=True)
+
+# Redirect the user to the URL when they click the link
+if st.button('Code Execution and Testing'):
+    st.experimental_redirect(url)
