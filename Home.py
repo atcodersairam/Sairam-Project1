da4be73ab9bd403b2deca57c861c55590690c52b
BN Sairam <139829666+atcodersairam@users.noreply.github.com>
2024-11-28 06:55:45 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,17 +11,16 @@ st.markdown(
     "[**Code Execution and Testing**](https://code-execution-modul-compiler-sairam-project1.streamlit.app/)"
 )
 
-# Add another hyperlink that redirects to the local host (port 8501)
+# Add another hyperlink with the updated label "Efficiency Analysis"
 st.markdown(
-    "[**Knowledge Extractor**](http://localhost:8501)"
+    "[**Efficiency Analysis**](https://efficiency-analysis-sairam-project1.streamlit.app/)"
 )
 
 # Add a button for Code Generation with Gemini that redirects to the specified URL
-
-
 st.markdown(
     "[**Code Generation With Gemini**](https://code-gen-gemini-sairam-project1.streamlit.app/)"
 )
+
 # Footer
 st.write("---")
 st.write("Created by Sairam - Project 1")
