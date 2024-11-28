447a8e647a0aa680c9acdf60ece2138b2c0e60de
BN Sairam <139829666+atcodersairam@users.noreply.github.com>
2024-11-28 06:33:27 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="Home Page - Knowledgeable Extractor", layout="centered")
 
 # Page content
-st.title("Knowledgeable Extractor")
+st.title("Home")
 
 # Add the first hyperlink (external URL)
 st.markdown(
